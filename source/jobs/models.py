21c7f31f37d33f1ba2be2cb3b56b580aae3844b9
Ryan Pitts <ryan.a.pitts@gmail.com>
2016-04-01 16:25:14 -0700

    cleanup for Django 1.5

--- source/jobs/models.py
+++ source/jobs/models.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import post_save
-from django.dispatch import receiver
 from django.template.defaultfilters import date as dj_date, slugify
 from django.utils.safestring import mark_safe
 
